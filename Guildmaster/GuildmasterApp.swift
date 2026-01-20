d4304df8cfe1683b7dae6bb84391b6ce5df6b654
tom <tommy.yohanes@icloud.com>
2026-01-20 21:08:15 +0700

    Initial Commit

---
+++ Guildmaster/GuildmasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuildmasterApp.swift
+//  Guildmaster
+//
+//  Created by tomtomtom on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GuildmasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
